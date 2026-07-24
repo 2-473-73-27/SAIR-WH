b75aa9833d4f16e5c14264e4affc0b4496f7c210
2-473-73-27 <sairahmadani5@gmail.com>
2026-07-24 14:44:40 +0500

    Change server port from 5000 to 3000

--- server.js
+++ server.js
@@ -48,7 +48,8 @@ app.get('/api/get-official-sms', (req, res) => {
     res.json({ success: true, data: liveOtpDatabase });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
+    
